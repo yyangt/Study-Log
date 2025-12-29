566726c89fc034c8a9e0d140e38084e501cd380e
yyangt <lizhideduiyou@gmail.com>
2025-12-29 23:49:15 +0800

    41

---
+++ Daily-Growth-2025/leetcode/110_平衡二叉树.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int height(struct TreeNode* root){
+    if (root==NULL){
+        return 0;
+    }
+    return fmax(height(root->left)+1,height(root->right)+1);
+}
+bool isBalanced(struct TreeNode* root) {
+    if(root==NULL){
+        return true;
+    }
+    return fabs(height(root->left) - height(root->right))<=1&&isBalanced(root->left)&&isBalanced(root->right);
+}
